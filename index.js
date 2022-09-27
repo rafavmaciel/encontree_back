1ec259db6e6e92e96e81af1d7df77ee6240208b0
Rafael <rafael.maciel@showtecnologia.com>
2022-09-27 10:35:25 -0300

    routes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const custonExpress = require('./src/config/custonExpress')
 const anuncio = require('./src/controllers/anuncio')
+const imovel = require('./src/controllers/imovel')
+const user = require('./src/controllers/user')
 require('dotenv').config();
 const port = process.env.PORT || 3003;
 
@@ -12,6 +14,10 @@ app.listen(port, function() {
 app.get("/", (req, res) => {
     res.send("Home");
   });
+
+//gambiarra para o consign com o vercel
 app.use(anuncio);
+app.use(imovel);
+app.use(user);
 
  
